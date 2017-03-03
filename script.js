02a3c51cf21662f39d938a2e9ac17df978e5454d
Noemi <andreoli.noemi@gmail.com>
2017-03-03 19:17:27 +0000

    done general static structure of the page

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+$(document).ready(function(){
+    $('.slider').slick({
+       dots: true,
+       infinite: true,
+       speed: 300,
+       slidesToShow: 1,
+       adaptiveHeight: true,
+       nextArrow: '<button type="button" class="arrow arrow-next"><img src="img/next.svg"></button>',
+       prevArrow: '<button type="button" class="arrow arrow-prev"><img src="img/back.svg"></button>'
+    });
+});
+   
