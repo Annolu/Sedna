d37228787e75b8ee0e05eb7311f3b00bf33d9e65
Noemi <andreoli.noemi@gmail.com>
2017-03-05 11:18:33 +0000

    sorted fixed top-menu animation

--- script.js
+++ script.js
@@ -9,5 +9,24 @@ $(document).ready(function(){
        nextArrow: '<button type="button" class="arrow arrow-next"><img src="img/next.svg"></button>',
        prevArrow: '<button type="button" class="arrow arrow-prev"><img src="img/back.svg"></button>'
     });
+    
+    //positions at which it shows/hides the fixed top-menu
+    
+        $(window).scroll(function() {  
+
+        let distanceFromTop= $(this).scrollTop();
+        
+        switch(true){
+            case (distanceFromTop<1):
+                $('.nav-main-container').removeClass('top-menu-down');            
+                break;   
+            case (distanceFromTop>1):
+                $('.nav-main-container').addClass('top-menu-down');
+                break;
+        }
+            
+            
+            
+    })
 });
    
