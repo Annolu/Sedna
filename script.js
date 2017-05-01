3e91fd8d9f7d6c76e254a65c3b4f35d7bfb8ea25
Noemi <andreoli.noemi@gmail.com>
2017-05-01 13:08:04 +0100

    added sass file and started refactoring

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    
+
     //slider
-    
+
     $('.slider').slick({
        dots: true,
        infinite: true,
@@ -12,16 +12,16 @@ $(document).ready(function(){
        nextArrow: '<button type="button" class="arrows arrow-next"><img src="img/next.svg"></button>',
        prevArrow: '<button type="button" class="arrows arrow-prev"><img src="img/back.svg"></button>'
     });
-    
+
     //tootip
-    
+
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
-    
-    
+
+
     $("#burger-container").on('click', function(){
-        
+
         //burger icon animation
 
         $(this).toggleClass("open");
@@ -33,80 +33,80 @@ $(document).ready(function(){
         blockBackgroundScroll();
 
     });
-    
+
     //no body scroll when black layer is visible
-    
+
     function blockBackgroundScroll(){
-        
+
         if($('.black-layer').hasClass('open')){
             $('body').addClass("fixed-position");
         }else{
             $('body').removeClass("fixed-position");
         }
     }
-    
+
     //columns order swap - on medium/small devices, the sliding-in devices images are positioned under the text content
-    
+
     function swapColumnOrder() {
 
         var windowWidth= $( window ).width();
-        
+
         if (windowWidth<990){
             $('.violet-section .content-column').addClass('flex-first');
         }else{
             $('.violet-section .content-column').removeClass('flex-first');
-            
+
             //on large devices, the black top menu if open it diseappears
-            
+
             $('.black-layer').removeClass("open");
-            $('.movidas').removeClass("open");
+            $('.nav-menu').removeClass("open");
             $("#burger-container").removeClass("open");
         }
     }
 
     swapColumnOrder();
-    
+
     $(window).resize(swapColumnOrder);
-    
-    $(window).scroll(function() {  
-        
+
+    $(window).scroll(function() {
+
         let distanceFromTop= $(this).scrollTop();
-        
+
         if(distanceFromTop>1){
             $('.nav-main-container').addClass('topmenu-fixed');
             $('.top-nav-back').addClass('bg-blue');
             $('.logo').addClass('logo_open');
-            $('.burger-container').addClass('burger-container_open');
+            $('.burger-container').addClass('burger-open');
         }else{
             $('.nav-main-container').removeClass('topmenu-fixed');
             $('.top-nav-back').removeClass('bg-blue');
             $('.logo').removeClass('logo_open');
-            $('.burger-container').removeClass('burger-container_open');
+            $('.burger-container').removeClass('burger-open');
         }
-        
+
         var whiteSection= $('.white-section')[0];
         var violetSection= $('.violet-section')[0];
-        
+
         var ipadFadeInPoint= whiteSection.offsetHeight + whiteSection.clientHeight/2;
-        var iphoneFadeInPoint= ipadFadeInPoint + 200;        
+        var iphoneFadeInPoint= ipadFadeInPoint + 200;
         var macbookFadeInPoint= violetSection.offsetHeight + violetSection.clientHeight/2;
-        
-        //img devices slide-in 
-        
+
+        //img devices slide-in
+
         if(distanceFromTop>ipadFadeInPoint){
-            $('#ipad').addClass('fade-in');            
+            $('#ipad').addClass('fade-in');
         }
-        
+
         if(distanceFromTop>iphoneFadeInPoint){
             $('#iphone').addClass('fade-in');
         }
-        
+
         if(distanceFromTop>macbookFadeInPoint){
             $('#macbook').addClass('slide-in');
         }
-            
+
         //smooth scroll to top when click on footer box-arrow
-        
+
         $('a[href^="#"]').on('click',function (e) {
             e.preventDefault();
 
@@ -114,7 +114,6 @@ $(document).ready(function(){
                 'scrollTop': 1,
                 behavior: 'smooth'
             }, 1200, 'swing');
-        })                
+        })
     })
 });
-   
