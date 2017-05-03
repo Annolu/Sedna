427895b7e63b2963f3a3ac5d8d8c66d22c0cf22d
Noemi <andreoli.noemi@gmail.com>
2017-05-03 13:10:53 +0100

    project done

--- script.js
+++ script.js
@@ -1,119 +1,119 @@
 $(document).ready(function(){
 
-    //slider
+  preloaderFadeOutTime= 500;
+  function hidePreloader(){
+      var preloader= $('#spinnerWrapper');
+      preloader.fadeOut(preloaderFadeOutTime)
+  }
+  hidePreloader();
 
-    $('.slider').slick({
-       dots: true,
-       infinite: true,
-       speed: 300,
-       slidesToShow: 1,
-       cssEase: 'ease',
-       adaptiveHeight: true,
-       nextArrow: '<button type="button" class="arrows arrow-next"><img src="img/next.svg"></button>',
-       prevArrow: '<button type="button" class="arrows arrow-prev"><img src="img/back.svg"></button>'
-    });
+  //slider
+  $('.slider').slick({
+     dots: true,
+     infinite: true,
+     speed: 300,
+     slidesToShow: 1,
+     cssEase: 'ease',
+     adaptiveHeight: true,
+     nextArrow: '<button type="button" class="arrows arrow-next"><img src="img/next.svg"></button>',
+     prevArrow: '<button type="button" class="arrows arrow-prev"><img src="img/back.svg"></button>'
+  });
 
-    //tootip
+  //tootip
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
+  $('.navigation-tab').on('click', function(){
+    closeMenu();
+  });
 
+  $('.right-buttons').on('click', function(){
+    closeMenu();
+  });
 
-    $("#burger-container").on('click', function(){
+  $("#burgerContainer").on('click', function(){
 
-        //burger icon animation
+    //burger icon animation
+    $(this).toggleClass("open");
 
-        $(this).toggleClass("open");
-
-        //top menu black layer on small/medium devices
-
-        $('.black-layer').toggleClass("open");
-        $('.nav-menu').toggleClass("open");
-        blockBackgroundScroll();
-
-    });
-
+    //top menu black layer on small/medium devices
+    $('#blackLayer').toggleClass("open");
+    $('#navMenu').toggleClass("open");
     //no body scroll when black layer is visible
+    $('body').toggleClass("no-scroll");
+  });
 
-    function blockBackgroundScroll(){
+  //columns order swap - on medium/small devices, the sliding-in devices images are positioned under the text content
+  swapColumnOrder();
 
-        if($('.black-layer').hasClass('open')){
-            $('body').addClass("fixed-position");
-        }else{
-            $('body').removeClass("fixed-position");
-        }
+  function swapColumnOrder() {
+    var windowWidth= $( window ).width();
+
+    if (windowWidth<990){
+      $('.violet-section .content-column').addClass('flex-first');
+    }else{
+      $('.violet-section .content-column').removeClass('flex-first');
+
+      //on large devices, the black top menu if open it diseappears
+    closeMenu();
+    }
+  }
+
+  function closeMenu(){
+    $('#blackLayer').removeClass("open");
+    $('#navMenu').removeClass("open");
+    $('#burgerContainer').removeClass("open");
+    $('body').removeClass("no-scroll");
+  }
+
+  $(window).resize(swapColumnOrder);
+
+  $(window).scroll(function() {
+    let distanceFromTop= $(this).scrollTop();
+
+    //fixed topmenu when scroll down
+    if(distanceFromTop>20){
+      $('#topMenu').addClass('topmenu-fixed');
+      $('#topNavBack').addClass('bg-blue');
+      $('#logo').addClass('logo-up');
+      $('#burgerContainer').addClass('burger-up');
+    }else{
+      $('#topMenu').removeClass('topmenu-fixed');
+      $('#topNavBack').removeClass('bg-blue');
+      $('#logo').removeClass('logo-up');
+      $('#burgerContainer').removeClass('burger-up');
     }
 
-    //columns order swap - on medium/small devices, the sliding-in devices images are positioned under the text content
+    var whiteSection= $('#whiteSection')[0];
+    var violetSection= $('#violetSection')[0];
 
-    function swapColumnOrder() {
+    var ipadFadeInPoint= whiteSection.offsetHeight + whiteSection.clientHeight/2;
+    var iphoneFadeInPoint= ipadFadeInPoint + 200;
+    var macbookFadeInPoint= violetSection.offsetHeight + violetSection.clientHeight/2;
 
-        var windowWidth= $( window ).width();
-
-        if (windowWidth<990){
-            $('.violet-section .content-column').addClass('flex-first');
-        }else{
-            $('.violet-section .content-column').removeClass('flex-first');
-
-            //on large devices, the black top menu if open it diseappears
-
-            $('.black-layer').removeClass("open");
-            $('.nav-menu').removeClass("open");
-            $("#burger-container").removeClass("open");
-        }
+    //img devices slide-in
+    if(distanceFromTop>ipadFadeInPoint){
+      $('#ipad').addClass('fade-in');
     }
 
-    swapColumnOrder();
+    if(distanceFromTop>iphoneFadeInPoint){
+      $('#iphone').addClass('fade-in');
+    }
 
-    $(window).resize(swapColumnOrder);
+    if(distanceFromTop>macbookFadeInPoint){
+      $('#macbook').addClass('slide-in');
+    }
 
-    $(window).scroll(function() {
+    //smooth scroll to top when click on footer box-arrow
 
-        let distanceFromTop= $(this).scrollTop();
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
 
-        if(distanceFromTop>1){
-            $('.nav-main-container').addClass('topmenu-fixed');
-            $('.top-nav-back').addClass('bg-blue');
-            $('.logo').addClass('logo_open');
-            $('.burger-container').addClass('burger-open');
-        }else{
-            $('.nav-main-container').removeClass('topmenu-fixed');
-            $('.top-nav-back').removeClass('bg-blue');
-            $('.logo').removeClass('logo_open');
-            $('.burger-container').removeClass('burger-open');
-        }
-
-        var whiteSection= $('.white-section')[0];
-        var violetSection= $('.violet-section')[0];
-
-        var ipadFadeInPoint= whiteSection.offsetHeight + whiteSection.clientHeight/2;
-        var iphoneFadeInPoint= ipadFadeInPoint + 200;
-        var macbookFadeInPoint= violetSection.offsetHeight + violetSection.clientHeight/2;
-
-        //img devices slide-in
-
-        if(distanceFromTop>ipadFadeInPoint){
-            $('#ipad').addClass('fade-in');
-        }
-
-        if(distanceFromTop>iphoneFadeInPoint){
-            $('#iphone').addClass('fade-in');
-        }
-
-        if(distanceFromTop>macbookFadeInPoint){
-            $('#macbook').addClass('slide-in');
-        }
-
-        //smooth scroll to top when click on footer box-arrow
-
-        $('a[href^="#"]').on('click',function (e) {
-            e.preventDefault();
-
-            $('body').stop().animate({
-                'scrollTop': 1,
-                behavior: 'smooth'
-            }, 1200, 'swing');
-        })
+      $('html,body').stop().animate({
+          'scrollTop': 1,
+          behavior: 'smooth'
+      }, 1200, 'swing');
     })
+  })
 });
